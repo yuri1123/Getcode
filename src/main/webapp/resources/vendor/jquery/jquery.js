612f4a4801462a2e9d70b1b24a574cfa4d52877c
Choi Yuri <yurifrom1123@gmail.com>
2023-04-16 02:35:40 +0900

    - 관리자 asset 추가 - 메인페이지 수정

--- src/main/webapp/resources/vendor/jquery/jquery.js
+++ src/main/webapp/resources/vendor/jquery/jquery.js
@@ -18,6 +18,7 @@
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 
 		// For CommonJS and CommonJS-like environments where a proper `window`
+
 		// is present, execute the factory and get jQuery.
 		// For environments that do not have a `window` with a `document`
 		// (such as Node.js), expose a factory as module.exports.
